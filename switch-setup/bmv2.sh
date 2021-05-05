34b3a31de54e0bc115ff05fe7cdb93ce4bc85d9c
Guilherme Matos <guimvmatos1@outlook.com>
2021-05-05 05:28:04 -0300

    Update bmv2.sh

--- switch-setup/bmv2.sh
+++ switch-setup/bmv2.sh
@@ -4,5 +4,16 @@ sudo ip link set dev lo up
 sudo ip link set dev enp0s9 up
 sudo ip link set dev enp0s10 up
 
+apt-get install curl
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+python get-pip.py
+
+cd /home/p4/behavioral-model/
+./configure --enable-debugger
+make
+make install
+
+
 p4c -b bmv2 ran.p4 -o ./
-nohup simple_switch -i 1@enp0s9 -i 2@enp0s10 ran.json &
+simple_switch --log-console -i 1@enp0s9 -i 2@enp0s10 ran.json
+cat commands2.txt | simple_switch_CLI
