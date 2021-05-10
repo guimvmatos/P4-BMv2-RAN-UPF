c6681dd3ba76a9a29a0206284325ecbd18ab3cc3
Guilherme Matos <guimvmatos1@outlook.com>
2021-05-10 04:36:53 -0300

    Update bmv2.sh

--- switch-setup/bmv2.sh
+++ switch-setup/bmv2.sh
@@ -4,6 +4,9 @@ sudo ip link set dev lo up
 sudo ip link set dev enp0s9 up
 sudo ip link set dev enp0s10 up
 
+ip link set enp0s9 mtu 1500
+ip link set enp0s10 mtu 9000
+
 apt-get install curl
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 python get-pip.py
@@ -16,5 +19,6 @@ make install
 
 
 p4c -b bmv2 ran.p4 -o ./
-simple_switch --log-console -i 1@enp0s9 -i 2@enp0s10 ran.json
+#simple_switch --log-console -i 1@enp0s9 -i 2@enp0s10 ran.json
+simple_switch -i 1@enp0s9 -i 2@enp0s10 ran.json
 cat commands2.txt | simple_switch_CLI
